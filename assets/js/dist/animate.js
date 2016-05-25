710467a28b67ad1a1997154fd7aa1ab9df9be6cd
Josh Johnson <josh.johnson@browserlondon.com>
2016-05-25 10:42:37 +0100

    Update dependencies

--- assets/js/dist/animate.js
+++ assets/js/dist/animate.js
@@ -1,4 +1,4 @@
-/*! animate.js v1.2.4 | (c) 2016 Josh Johnson | https://github.com/jshjohnson/animate.js */
+/*! animate.js v1.2.5 | (c) 2016 Josh Johnson | https://github.com/jshjohnson/animate.js */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory(root));
