af73a95cd95bcabcb36aad459512e04c75abda26
Josh Johnson <josh.johnson@browserlondon.com>
2016-08-10 13:45:18 +0100

    Fix UMD wrapper bug for RequireJS

--- assets/js/dist/animate.js
+++ assets/js/dist/animate.js
@@ -1,13 +1,13 @@
 /*! animate.js v1.2.5 | (c) 2016 Josh Johnson | https://github.com/jshjohnson/animate.js */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory(root));
-    } else if (typeof exports === 'object') {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports === "object") {
         module.exports = factory(root);
     } else {
         root.Animate = factory(root);
     }
-})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
+})(typeof global !== 'undefined' ? global : this.window || this.global, function(root) {
 
     'use strict';
 
--- assets/js/src/animate.js
+++ assets/js/src/animate.js
@@ -1,12 +1,12 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory(root));
-    } else if (typeof exports === 'object') {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports === "object") {
         module.exports = factory(root);
     } else {
         root.Animate = factory(root);
     }
-})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
+})(typeof global !== 'undefined' ? global : this.window || this.global, function(root) {
 
     'use strict';
 
