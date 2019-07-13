85db2b1ac25782ae0ebf57564dfe87137af346f1
Josh Johnson <josh@joshuajohnson.co.uk>
2019-07-13 13:21:58 +0100

    Add uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,9 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import {
+  uglify
+} from 'rollup-plugin-uglify';
 
 export default {
   input: './src/js/animate.ts',
@@ -7,5 +12,10 @@ export default {
     name: 'Animate',
     dir: 'dist',
   },
-  plugins: [typescript()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    uglify(),
+  ],
 };
